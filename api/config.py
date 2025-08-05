a9862ad648eeda03c2323a6bb83b74b59b6577f2
Sriyarepaka <repakasriyad04@gmail.com>
2025-08-05 23:52:52 +0530

    Initial commit

--- api/config.py
+++ api/config.py
@@ -7,13 +7,13 @@ class Settings(BaseSettings):
     """Application settings."""
     # API config
     API_V1_STR: str = "/api"
-    PROJECT_NAME: str = "Bazario"
+    PROJECT_NAME: str = "trashtocash"
     
     # Database connection
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: str = os.getenv("DB_PORT", "5432")
-    DB_USER: str = os.getenv("DB_USER", "yeedu")
-    DB_PASS: str = os.getenv("DB_PASS", "yeedu")
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASS: str = os.getenv("DB_PASS", "sriya")
     DB_NAME: str = os.getenv("DB_NAME", "postgres")
     
     # JWT
